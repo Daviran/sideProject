64208d859ace95cfcf2eb6a0962fd6070422a20c
David BUGNON <david_bugnon@tgims.com>
2024-05-02 11:46:56 +0200

    feat: can view content of a text input file

--- baplieEditor/src/views/BaplieViewer.vue
+++ baplieEditor/src/views/BaplieViewer.vue
@@ -1,13 +1,38 @@
-<script setup>
-// import Baplie from '../components/Baplie.vue'
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const fileContent = ref(null);
+
+    const handleFileChange = (event) => {
+      const file = event.target.files[0];
+      if (file && file.type === 'text/plain') {
+        const reader = new FileReader();
+        reader.onload = () => {
+          fileContent.value = reader.result;
+        };
+        reader.readAsText(file);
+      } else {
+        // Handle error if file is not txt
+        console.error('Please select a valid text file.');
+      }
+    };
+
+    return {
+      fileContent,
+      handleFileChange
+    };
+  }
+};
 </script>
 
 <template>
-  <main>
-    <div>
-          <h1>Load Baplie</h1>
+  <div>
+    <input type="file" @change="handleFileChange" accept=".txt">
+    <div v-if="fileContent !== null">
+      <h2>Baplie Content</h2>
+      <pre>{{ fileContent }}</pre>
     </div>
-
-    <baplie />
-  </main>
+  </div>
 </template>
