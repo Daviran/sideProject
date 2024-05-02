d190d07a89987b172191f53199cae408e048919c
David BUGNON <david_bugnon@tgims.com>
2024-05-02 12:06:54 +0200

    feat: can save modified baplie & better structure

--- baplieEditor/src/components/BaplieFile.vue
+++ baplieEditor/src/components/BaplieFile.vue
@@ -1,7 +1,59 @@
-<script setup>
+<script>
+import { ref } from 'vue';
 
+export default {
+  setup() {
+    const textObject = ref(null);
+    const handleFileChange = (event) => {
+      const file = event.target.files[0];
+      if (file && file.type === 'text/plain') {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const text = reader.result;
+          textObject.value = { content: text};
+        };
+        reader.readAsText(file);
+      } else {
+        // Handle error if file is not txt
+        console.error('Please select a valid text file.');
+      }
+    };
+    const handleInput =(e) => {
+        if(textObject.value) {
+            textObject.value.content = e.target.innerText;
+        }
+    };
+    const saveAsTxt = () => {
+         if (textObject.value) {
+        const blob = new Blob([textObject.value.content], { type: 'text/plain' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'modified_baplie.txt';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+      }
+    }
+
+    return {
+        textObject,
+        handleFileChange,
+        handleInput,
+        saveAsTxt
+    };
+  }
+};
 </script>
 
 <template>
- <h1>BaplieFILE</h1>
+  <div>
+    <input type="file" @change="handleFileChange" accept=".txt">
+    <div v-if="textObject !== null">
+      <h2>Baplie Content</h2>
+      <pre contenteditable="true" @input="handleInput">{{ textObject.content }}</pre>
+      <button @click="saveAsTxt">Save modified Baplie file</button>
+    </div>
+  </div>
 </template>
--- baplieEditor/src/views/BaplieViewer.vue
+++ baplieEditor/src/views/BaplieViewer.vue
@@ -1,38 +1,13 @@
-<script>
-import { ref } from 'vue';
+<script setup>
+import BaplieFile from "../components/BaplieFile.vue"
 
-export default {
-  setup() {
-    const fileContent = ref(null);
-
-    const handleFileChange = (event) => {
-      const file = event.target.files[0];
-      if (file && file.type === 'text/plain') {
-        const reader = new FileReader();
-        reader.onload = () => {
-          fileContent.value = reader.result;
-        };
-        reader.readAsText(file);
-      } else {
-        // Handle error if file is not txt
-        console.error('Please select a valid text file.');
-      }
-    };
-
-    return {
-      fileContent,
-      handleFileChange
-    };
-  }
-};
 </script>
 
 <template>
   <div>
-    <input type="file" @change="handleFileChange" accept=".txt">
-    <div v-if="fileContent !== null">
-      <h2>Baplie Content</h2>
-      <pre>{{ fileContent }}</pre>
-    </div>
+   <baplie-file>
+
+
+   </baplie-file>
   </div>
 </template>
