0ebf7da41f1924ec6fc63a3fc2d553287d9d8644
Daviran <dbugnon@outlook.fr>
2024-05-17 12:25:46 +0200

    feat: first working version of the BAPLIE Parser in Java!

--- ediApi/src/main/java/edi/editor/ediApi/controller/EdiController.java
+++ ediApi/src/main/java/edi/editor/ediApi/controller/EdiController.java
@@ -1,5 +1,6 @@
 package edi.editor.ediApi.controller;
 
+import edi.editor.ediApi.utils.BaplieParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,144 +8,16 @@ import org.springframework.http.ResponseEntity;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class EdiController {
 
-
-    public static class Port {
-        public String code;
-        public String name;
-
-        public Port(String code, String name) {
-            this.code = code;
-            this.name = name;
-        }
-    }
-
-    public static class Container {
-        public String number;
-        public Port portOfLoading;
-        public Port portOfDestination;
-        public Port portOfDischarge;
-        public String containerCarrier;
-        public String grossWeight;
-        public String vgmWeight;
-    }
-
-    public static class BaplieData {
-        public List<Container> containers = new ArrayList<>();
-    }
-
     @PostMapping("/parse")
     public ResponseEntity<String> parseEDI(@RequestParam("file") MultipartFile file) {
-        BaplieData baplieData = new BaplieData();
-        Container currentContainer = null;
-
-        try (InputStream is = file.getInputStream();
-             BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-
-            List<String> segments = new ArrayList<>();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                segments.add(line);
-            }
-
-            // Process segments in reverse order
-            for (int i = segments.size() - 1; i >= 0; i--) {
-                String segment = segments.get(i).trim();
-                System.out.println("Processing segment: " + segment); // Debug statement
-
-                // Split the segment using the '+' delimiter
-                String[] elements = segment.split("\\+");
-                if (elements.length == 0) continue;
-
-                String segmentId = elements[0];
-                System.out.println("Segment ID: " + segmentId); // Debug statement
-                System.out.println("Elements: " + java.util.Arrays.toString(elements)); // Debug statement
-
-                switch (segmentId) {
-                    case "EQD":
-                        if (elements.length > 2) {
-                            String containerNumber = elements[2].trim();
-                            currentContainer = new Container();
-                            currentContainer.number = containerNumber;
-                            baplieData.containers.add(currentContainer);
-                            System.out.println("New container added: " + containerNumber); // Debug statement
-                        }
-                        break;
-
-                    case "LOC":
-                        if (elements.length > 2) {
-                            String locationCode = elements[1];
-                            String locationName = elements[2].split(":")[0].trim();
-                            if (currentContainer != null) {
-                                switch (locationCode) {
-                                    case "83":
-                                        if (currentContainer.portOfDestination == null) {
-                                            currentContainer.portOfDestination = new Port(locationCode, locationName);
-                                            System.out.println("POD updated: " + locationName); // Debug statement
-                                        }
-                                        break;
-                                    case "9":
-                                        if (currentContainer.portOfLoading == null) {
-                                            currentContainer.portOfLoading = new Port(locationCode, locationName);
-                                            System.out.println("POL updated: " + locationName); // Debug statement
-                                        }
-                                        break;
-                                    case "11":
-                                        if (currentContainer.portOfDischarge == null) {
-                                            currentContainer.portOfDischarge = new Port(locationCode, locationName);
-                                            System.out.println("POD updated: " + locationName); // Debug statement
-                                        }
-                                        break;
-                                }
-                            }
-                        }
-                        break;
-
-                    case "NAD":
-                        if (elements.length > 2) {
-                            String carrierCode = elements[2].split(":")[0].trim();
-                            if (currentContainer != null) {
-                                currentContainer.containerCarrier = carrierCode;
-                                System.out.println("Carrier updated: " + carrierCode); // Debug statement
-                            }
-                        }
-                        break;
-
-                    case "MEA":
-                        if (elements.length > 3) {
-                            String measurementCode = elements[2].trim();
-                            String[] measurementParts = elements[3].split(":");
-                            if (measurementParts.length > 1) {
-                                String measurementValue = measurementParts[1].trim();
-                                if (currentContainer != null) {
-                                    if (measurementCode.equals("KGM")) {
-                                        currentContainer.vgmWeight = measurementValue;
-                                        System.out.println("VGM weight updated: " + measurementValue); // Debug statement
-                                    } else if (measurementCode.equals("AAE")) {
-                                        currentContainer.grossWeight = measurementValue;
-                                        System.out.println("Gross weight updated: " + measurementValue); // Debug statement
-                                    }
-                                }
-                            }
-                        }
-                        break;
-                }
-            }
-
-            // Reverse the order of containers to reflect the correct order
-            List<Container> reversedContainers = new ArrayList<>();
-            for (int i = baplieData.containers.size() - 1; i >= 0; i--) {
-                reversedContainers.add(baplieData.containers.get(i));
-            }
-            baplieData.containers = reversedContainers;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            BaplieParser.BaplieData baplieData = BaplieParser.parse(reader);
 
             // Convert the BaplieData object to a JSON string
             ObjectMapper mapper = new ObjectMapper();
---
+++ ediApi/src/main/java/edi/editor/ediApi/utils/BaplieParser.java
@@ -0,0 +1,137 @@
+package edi.editor.ediApi.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaplieParser {
+
+    public static class Port {
+        public String code;
+        public String name;
+
+        public Port(String code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+    }
+
+    public static class Container {
+        public String number;
+        public Port portOfLoading;
+        public Port portOfDestination;
+        public Port portOfDischarge;
+        public String containerCarrier;
+        public String grossWeight;
+        public String vgmWeight;
+    }
+
+    public static class BaplieData {
+        public List<Container> containers = new ArrayList<>();
+    }
+
+    public static BaplieData parse(BufferedReader reader) throws IOException {
+        BaplieData baplieData = new BaplieData();
+        Container currentContainer = null;
+
+        List<String> segments = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            segments.add(line);
+        }
+
+        // Process segments in reverse order
+        for (int i = segments.size() - 1; i >= 0; i--) {
+            String segment = segments.get(i).trim();
+            System.out.println("Processing segment: " + segment); // Debug statement
+
+            // Split the segment using the '+' delimiter
+            String[] elements = segment.split("\\+");
+            if (elements.length == 0) continue;
+
+            String segmentId = elements[0];
+            System.out.println("Segment ID: " + segmentId); // Debug statement
+            System.out.println("Elements: " + java.util.Arrays.toString(elements)); // Debug statement
+
+            switch (segmentId) {
+                case "EQD":
+                    if (elements.length > 2) {
+                        String containerNumber = elements[2].trim();
+                        currentContainer = new Container();
+                        currentContainer.number = containerNumber;
+                        baplieData.containers.add(currentContainer);
+                        System.out.println("New container added: " + containerNumber); // Debug statement
+                    }
+                    break;
+
+                case "LOC":
+                    if (elements.length > 2) {
+                        String locationCode = elements[1];
+                        String locationName = elements[2].split(":")[0].trim();
+                        if (currentContainer != null) {
+                            switch (locationCode) {
+                                case "83":
+                                    if (currentContainer.portOfDestination == null) {
+                                        currentContainer.portOfDestination = new Port(locationCode, locationName);
+                                        System.out.println("POD updated: " + locationName); // Debug statement
+                                    }
+                                    break;
+                                case "9":
+                                    if (currentContainer.portOfLoading == null) {
+                                        currentContainer.portOfLoading = new Port(locationCode, locationName);
+                                        System.out.println("POL updated: " + locationName); // Debug statement
+                                    }
+                                    break;
+                                case "11":
+                                    if (currentContainer.portOfDischarge == null) {
+                                        currentContainer.portOfDischarge = new Port(locationCode, locationName);
+                                        System.out.println("POD updated: " + locationName); // Debug statement
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+
+                case "NAD":
+                    if (elements.length > 2) {
+                        String carrierCode = elements[2].trim().split(":")[0];
+                        if (currentContainer != null) {
+                            currentContainer.containerCarrier = carrierCode;
+                            System.out.println("Carrier updated: " + carrierCode); // Debug statement
+                        }
+                    }
+                    break;
+
+                case "MEA":
+                    if (elements.length > 3) {
+                        String measurementCode = elements[1].trim();
+                        String[] measurementParts = elements[3].split(":");
+                        if (measurementParts.length > 1) {
+                            String measurementValue = measurementParts[1].trim();
+                            if (currentContainer != null) {
+                                if (measurementCode.equals("AAE")) {
+                                    currentContainer.grossWeight = measurementValue;
+                                    System.out.println("Gross weight updated: " + measurementValue); // Debug statement
+                                } else if (measurementCode.equals("KGM") || measurementCode.equals("VGM")) {
+                                    currentContainer.vgmWeight = measurementValue;
+                                    System.out.println("VGM weight updated: " + measurementValue); // Debug statement
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+
+        // Reverse the order of containers to reflect the correct order
+        List<Container> reversedContainers = new ArrayList<>();
+        for (int i = baplieData.containers.size() - 1; i >= 0; i--) {
+            reversedContainers.add(baplieData.containers.get(i));
+        }
+        baplieData.containers = reversedContainers;
+
+        return baplieData;
+    }
+}
