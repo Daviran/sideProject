3951b93287184d7a6a07762f502b9cbfbea77235
Daviran <dbugnon@outlook.fr>
2024-05-17 07:57:50 +0200

    wip:backend API that will parse the EDIs using STAEDI lib in java

---
+++ ediApi/src/main/java/edi/editor/sideproject/SideprojectApplication.java
@@ -0,0 +1,13 @@
+package edi.editor.ediapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ediapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ediapiApplication.class, args);
+	}
+
+}
---
+++ ediApi/src/test/java/edi/editor/sideproject/SideprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package edi.editor.ediapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ediapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
